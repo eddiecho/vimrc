963d70828fa9ac9d6d007a3b55c462a5808f57c2
Edward Cho <eunseocho@gmail.com>
2021-04-07 22:30:46 -0700

    brand new config using lua

---
+++ lua/mappings/lua.lua
@@ -0,0 +1,46 @@
+-- General key mappings
+
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Move vertically by visual line
+map("", "j", "gj")
+map("", "k", "gk")
+map("", "<Down>", "gj")
+map("", "<Up>", "gk")
+
+-- Center search results
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
+
+-- Clear last search with Enter
+map("n", "<CR>", ":noh<CR><CR>")
+
+-- Because I'm a bad typist
+map("c", "W", "w")
+map("c", "Wq", "wq")
+map("c", "WQ", "wq")
+map("c", "Q", "q")
+map("c", "Qa", "qa")
+map("c", "Wqa", "wqa")
+map("c", "WQa", "wqa")
+map("c", "Wa", "wa")
+map("c", "WA", "wa")
+map("c", "qw", "wq")
+map("c", "qwa", "wqa")
+map("c", "Qw", "wq")
+map("c", "Qwa", "wqa")
+
+-- Esc map
+map("i", "kj", "<Esc>")
+
+-- Split navigation
+map("n", "H", "<C-w>h")
+map("n", "L", "<C-w>l")
+map("n", "K", "<C-w>k")
+map("n", "J", "<C-w>j")
---
+++ lua/misc-utils/lua.lua
@@ -0,0 +1,50 @@
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+local function opt(scope, key, value)
+    scopes[scope][key] = value
+    if scope ~= "o" then
+        scopes["o"][key] = value
+    end
+end
+
+-- Buffers become hidden when abandoned
+opt("o", "hidden", true)
+-- Ignore case on search
+opt("o", "ignorecase", true)
+-- Better splits
+opt("o", "splitbelow", true)
+opt("o", "splitright", true)
+-- Line numbers
+opt("w", "number", true)
+opt("o", "numberwidth", 2)
+-- Highlight current line
+opt("w", "cul", true)
+-- Allow mouse
+opt("o", "mouse", "a")
+-- Signs next to line numbers
+opt("w", "signcolumn", "yes")
+-- Height of status line
+opt("o", "cmdheight", 1)
+-- Update delay time
+opt("o", "updatetime", 250)
+-- Allow copy paste
+opt("o", "clipboard", "unnamedplus")
+-- Tabs are just spaces
+opt("b", "expandtab", true)
+opt("b", "shiftwidth", 2)
+-- Colors
+opt("o", "termguicolors", true)
+
+local M = {}
+
+function M.is_buffer_empty()
+    -- Check whether the current buffer is empty
+    return vim.fn.empty(vim.fn.expand("%:t")) == 1
+end
+
+function M.has_width_gt(cols)
+    -- Check if the windows width is greater than a given number of columns
+    return vim.fn.winwidth(0) / 2 > cols
+end
+
+return M
